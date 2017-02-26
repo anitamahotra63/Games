261d417b780ca45b65cc6854030d76d9d2e6e7ae
anitamahotra63 <anitamahotra63@gmail.com>
2017-02-26 12:47:07 +0530

    Create client.java

---
+++ Tic-Tac-Toe/client.java
@@ -0,0 +1,13 @@
+package lect22.tic_tac_toe;
+
+public class client {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		TTT t=new TTT();
+		t.start_game();
+
+	}
+
+}
